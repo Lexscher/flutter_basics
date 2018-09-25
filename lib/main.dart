6354fdd2a9a3a1b87cb9de77937a89741c90385c
Alexander Schelchere <ascellie@gmail.com>
2018-09-25 10:44:27 -0400

    main: App Bar

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,12 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Flutter is Cool!'),
+          leading: new Icon(
+            Icons.camera_front
+          )
+        ),
         body: new Center(
           child: new Text('Hello World!'),
         ),
